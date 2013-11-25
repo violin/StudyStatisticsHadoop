3a7c45ef76e73c5a0b0b3c73f0f7322286a34dab
hzyangzhou <hzyangzhou@corp.netease.com>
2013-11-25 17:44:18 +0800

    init

---
+++ src/com/netease/edu/hadoop/SpiderCount.java
@@ -0,0 +1,83 @@
+package com.netease.edu.hadoop;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class SpiderCount {
+	public static final String SPIDER_REGEX = "(Baiduspider|Googlebot)";
+	
+	public static class TokenizerMapper extends
+			Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable one = new IntWritable(1);
+		private Pattern p = Pattern.compile(SPIDER_REGEX);
+
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+//			StringTokenizer itr = new StringTokenizer(value.toString());
+//			while (itr.hasMoreTokens()) {
+//				line.set(itr.nextToken());
+//				context.write(line, one);
+//			}
+			String currentLine = value.toString();
+			Matcher matcher = p.matcher(currentLine);
+			if(matcher.find()){
+				String[] split = currentLine.split(" ");
+				String url = split[6];
+				if(url.indexOf("?")>0){
+					url = url.substring(0,url.indexOf("?"));
+				}
+				url = matcher.group(0) + url;
+				Text urlText = new Text(url);
+				context.write(urlText, one);
+			}
+		}
+	}
+
+	public static class IntSumReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+				.getRemainingArgs();
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: wordcount <in> <out>");
+			System.exit(2);
+		}
+		Job job = new Job(conf, "word count");
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
